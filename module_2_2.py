58ec77cdc6fdd391bbdba2911c09de0654982973
Alena <89519703610alena@gmail.com>
2024-09-11 20:22:49 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = 2
+second = 2
+third = 2
+print( first, second, third )
+if first == second == third:
+    print(3)
+elif first == second or second == third:
+    print(2)
+elif first != second != third:
+    print(0)
