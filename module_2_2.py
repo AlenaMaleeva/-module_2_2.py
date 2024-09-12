ceb29f02d5a49410f48e0eb2cb63dbb416442b09
Alena <89519703610alena@gmail.com>
2024-09-12 19:36:18 +0500

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -4,7 +4,7 @@ third = 2
 print( first, second, third )
 if first == second == third:
     print(3)
-elif first == second or second == third:
+elif first == second or second == third or first == third:
     print(2)
 elif first != second != third:
     print(0)
